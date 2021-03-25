eb7039c0cf429eb45417a11df70c657eaa3c205d
Anthony Ulloa <aulloan93@gmail.com>
2021-03-25 03:16:43 -0500

    anecdotes - step3

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -25,12 +25,31 @@ const App = () => {
     setVotes(copyVotes);
   }
 
+  function getIndexMostVoted() {
+    let index = 0;
+    votes.reduce((prevValue, currentValue, currentIndex) => {
+      if (currentValue > prevValue) {
+        index = currentIndex;
+        return currentValue;
+      }
+
+      return prevValue;
+    });
+
+    return index;
+  }
+
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
       <button onClick={() => voteAnecdote()}>vote</button>
       <button onClick={() => nextAnecdote()}>next anecdote</button>
+      <br></br>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[getIndexMostVoted()]}</p>
+      <p>has {votes[getIndexMostVoted()]} votes</p>
     </div>
   )
 };
