8eae20fa5249d6880dead2666382973f19e52d72
Anthony Ulloa <aulloan93@gmail.com>
2021-04-18 02:22:22 -0500

    part2 - courseinfo - step6

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -24,39 +24,38 @@ const Part = ({name, exercises}) => {
   );
 };
 
-const Total = ({parts}) => {
-	const total = parts.reduce((total, part) => total + part.exercises, 0);
-
-	return (
-		<p>Number of exercises {total}</p>
-	);
+const Course = ({course}) => {
+  return [
+    <Header key="header" course={course.name} />,
+    <Content key="content" parts={course.parts} />
+  ];
 };
 
 const App = () => {
-	const course = {
+  const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   };
 
 	return (
-		<div>
-			<Header course={course.name} />
-			<Content parts={course.parts} />
-			<Total parts={course.parts} />
-		</div>
+		<Course course={course} />
 	);
 };
 
